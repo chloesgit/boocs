5413c075dcfe85fa262ea73ca423eeb63ace19b4
EwenQuim <ewen.quimerch@gmail.com>
2020-11-11 00:37:20 +0100

    front: create library component

--- front-boocs/src/app/app.module.ts
+++ front-boocs/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LibraryComponent } from './library/library.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LibraryComponent
   ],
   imports: [
     BrowserModule,
